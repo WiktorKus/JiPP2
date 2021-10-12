0fd73321e6002845f46b87a9b36a230cf86e66be
Wiktor Kuś <wiktorex.kus@gmail.com>
2021-10-12 10:45:08 +0200

    lab2

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+int add(int argv[0], int argv[1]) {
+    return a + b;
+}
+
+int subtract(int argv[0], int argv[1]) {
+    return a - b;
+}
+
+int volume() {
+
+}
+
+void help() {
+    cout<<"";
+}
+
 int main(int argc, char *argv[]) {
     for(int i = 0; i < argc ; ++i) {
         cout << argv[i] <<endl;
