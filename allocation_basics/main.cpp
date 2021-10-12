374c3618d4ed1d6367896fa9a03dcc3cb9d312af
Wiktor Kuś <wiktorex.kus@gmail.com>
2021-10-12 10:37:54 +0200

    lab2

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-int main()
-{
-    cout<<"Hello World";
-    cout<<" ";
+int main(int argc, char *argv[]) {
+    for(int i = 0; i < argc ; ++i) {
+        cout << argv[i] <<endl;
+    }
 
     return 0;
 }
