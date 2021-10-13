fd649051beb31d60775cbb1e6a9bba7cb0900879
Wiktor Kuś <wiktorex.kus@gmail.com>
2021-10-13 20:06:31 +0200

    lab2

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -2,28 +2,12 @@
 
 using namespace std;
 
-int add(int argv[0], int argv[1]) {
-    return a + b;
-}
+int main()
+{
+    int telefon = 12345;    //zmienna liczbowa
+    int *wsk = &telefon;    //wskaźnik wsk zawiera adres zmiennej telefon
 
-int subtract(int argv[0], int argv[1]) {
-    return a - b;
-}
-
-int volume() {
-
-}
-
-void help() {
-    cout<<"";
-}
-
-int main(int argc, char *argv[]) {
-    for(int i = 0; i < argc ; ++i) {
-        cout << argv[i] <<endl;
-    }
+    cout << *wsk << endl;
 
     return 0;
 }
-
-
