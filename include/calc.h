20bad20fb05a5a410c795942fa8e2ee5f5a6e958
WiktorKus <91941867+WiktorKus@users.noreply.github.com>
2021-10-14 17:37:32 +0200

    Add files via upload

---
+++ include/calc.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLECALC_CALC_H
+#define SIMPLECALC_CALC_H
+
+//deklaracje funkcji
+
+int add(char *argv[]); //dodawanie dwoch liczb calkowitych
+int subtract(char *argv[]); //odejmowanie dwoch liczb calkowitych
+double volume(char *argv[]); //obliczanie objetosci graniastoslupa prostego o podstawie trapezu
+void help(); //dokumentacja dla funkcji
+
+#endif
