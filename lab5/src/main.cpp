1ac7ffbae8ce9d74ea83f2541f6778536cfb1429
Wiktor Kuś <wiktorex.kus@gmail.com>
2021-11-12 19:25:04 +0100

    lab5

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <../include/Car.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+
+int main () {
+
+    int placeNumber;
+    string passengerName;
+
+    Car myCar("AX 312", "Fiat", 5, "124p", "Kabriolet");
+    myCar.printInformation();
+
+    cout<<"\nPodaj numer miejsca w samochodzie: ";
+    cin>>placeNumber;
+    cout<<"\nPodaj imie i nazwisko pasazera: ";
+    cin>>passengerName;
+
+    myCar.changeInformationPassenger(placeNumber, passengerName);
+
 }
